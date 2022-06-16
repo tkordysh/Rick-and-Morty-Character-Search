8358b02c91acc1e90f628a04854124278a239dd0
Tony Kordysh <tkordysh@gmail.com>
2022-06-16 06:24:09 -0500

    changed display card

--- src/components/DisplaySinglePersonCard.jsx
+++ src/components/DisplaySinglePersonCard.jsx
@@ -4,7 +4,7 @@ import React from "react"
 
 const DisplaySinglePersonCard = (props) => {
 
-    // console.log(props);
+    console.log(props);
 
     const { person } = props;
     
@@ -13,8 +13,7 @@ const DisplaySinglePersonCard = (props) => {
          <article>
             <img src={person.image} alt={person.name}></img>
             <h2>{person.name}</h2>
-            <p>{person.name} is a {person.gender} {person.species} who is currently {person.status}</p>
-            <h3>Appears in the following episodes</h3>
+            <p>{person.name} is a {person.gender} {person.species} whose status is currently {person.status}</p>
          </article>
     )
 }
